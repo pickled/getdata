7537f64fa2d734b06f76885fba42dbd7ad780d78
nezah <nezahrish@gmail.com>
2014-04-27 23:15:41 +0900

    init

---
+++ run_analysis.R
@@ -0,0 +1,10 @@
+## Peer assessment
+
+setwd("/Users/nezah/Dropbox/private/study/coursera/getdata/project")
+"Dataset"
+
+fileUrl=""
+download.file(fileUrl, destfile="", method="curl")
+
+fileUrl=""
+download.file(fileUrl, destfile="", method="curl")
